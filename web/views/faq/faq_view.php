b11f2b3661e0411f4c47a157bccef9762f9afaa6
JOYCEELY XAVIER BARBOSA <16165855@CFP127ALUNO.LOCAL>
2017-09-28 16:31:20 -0300

    faq

--- web/views/faq/faq_view.php
+++ web/views/faq/faq_view.php
@@ -1,51 +1,54 @@
 
 			<main>
-					<div id = "faq">
-						<div id = "txtFaq">
-								Dúvidas Frequentes
-						</div>
-					</div>
-						<div id = "conteudoFAQ">
-								<div id = "duvidas">
-									<div id = "txtDuvidas">
-										<h1>Ainda ficou com dúvidas?</h1>
-									</div>
-								</div >
-								<div > <img id="imagemFaq" src="img/faq.png"> </div>
-								<div id = "contatosFaq">
-									<p>faq@theribs.com.br</p>
-										11 4141-8922 </div>
-								</div>
-					</div>
-				<!-- Div Accordion -->
-				<div id="perguntas">
-					<div class = "alinhamento">
-						<div class="pergunta">
-							<div ><a class="exibir">Pergunta?</a></div>
-						</div>
-						<div class = "resposta">
-							<div > Resposta </div>
-						</div>
-						<div class="pergunta">
-							<div ><a class="exibir">Pergunta?</a></div>
-						</div>
-						<div class = "resposta">
-							<div > Resposta </div>
-						</div>
-						<div class="pergunta">
-							<div ><a class="exibir">Pergunta?</a></div>
-						</div>
-						<div class = "resposta">
-							<div > Resposta </div>
-						</div>
-						<div class="pergunta">
-							<div ><a class="exibir">Pergunta?</a></div>
-						</div>
-						<div class = "resposta">
-							<div > Resposta </div>
+				<div >
+						<div id = "faq">
+							<div id = "txtFaq">
+									Dúvidas Frequentes
+							</div>
 						</div>
 
-						<div class="fechar_resposta"><a class="esconder">Fechar X </a></div>
+				</div>
+				<!-- Div Accordion -->
+				<div id="container_perguntas">
+					<div id="perguntas">
+						<div class = "alinhamento">
+							<div class="pergunta">
+								<div ><a class="exibir">Pergunta?</a></div>
+							</div>
+							<div class = "resposta">
+								<div > Resposta </div>
+							</div>
+							<div class="pergunta">
+								<div ><a class="exibir">Pergunta?</a></div>
+							</div>
+							<div class = "resposta">
+								<div > Resposta </div>
+							</div>
+							<div class="pergunta">
+								<div ><a class="exibir">Pergunta?</a></div>
+							</div>
+							<div class = "resposta">
+								<div > Resposta </div>
+							</div>
+							<div class="pergunta">
+								<div ><a class="exibir">Pergunta?</a></div>
+							</div>
+							<div class = "resposta">
+								<div > Resposta </div>
+							</div>
+
+							<div class="fechar_resposta"><a class="esconder">Fechar X </a></div>
+						</div>
+					</div>
+					<div id = "duvidas">
+						<div id = "txtDuvidas">
+							<h1>Ainda ficou com dúvidas?</h1>
+						</div>
+						<div> <img id="imagemFaq" src="img/faq.png"> </div>
+						<div id = "contatosFaq">
+							<p>faq@theribs.com.br</p>
+								11 4141-8922
+						</div>
 					</div>
 				</div>
 				<!-- Fim accordion -->
