f9bb88952d2c1799da65cdbf50c1fc61dc58079a
Giovanna Silva Resende <giovanna10silva@gmail.com>
2017-09-29 00:54:45 -0300

    cadastro em andamento

---
+++ web/head.php
@@ -0,0 +1,27 @@
+<head>
+  <meta charset="utf-8">
+  <title>The Ribs - SteakHouse </title>
+  <link rel="icon" href="img/logo_folha.png">
+
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_avalie.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_cadastro.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_cardapio.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_enquete.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_galeria_fotos.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_galeria_principal_cms.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_login.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_pagina_principal_cms.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_sobre_empresa.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_faq.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo_adquirir_reserva.css">
+
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/script_menu_lateral.js"></script>
+  <script type="text/javascript" src="js/script_menu_fixo.js"></script>
+  <script type="text/javascript" src="js/tabs.js"></script>
+  <script type="text/javascript" src="js/jquery.cycle.all.js"></script>
+  <script type="text/javascript" src="js/script-slide-show.js"></script>
+  <script type="text/javascript" src="js/jcarousellite.js"></script>
+</head>
--- web/models/cliente_class.php
+++ web/models/cliente_class.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
   /*
      ************************************************************************************************
      * Objetivo: Criar a modelagem para a manipulação dos dados com o BD
@@ -9,16 +9,50 @@
      * Modificações:
      * ************************************************************************************************
    */
+
+
   class ClienteLogar{
 
     public $nome_usuario;
     public $senha;
 
+    //Construtor da Classe
+    public function __construct(){
+
+        //Inclui o arquivo de conexao com o BD
+        require_once('models/banco_classe.php');
+
+        //Instancia a Classe Mysql_db
+        $conexao_bd = new Mysql_db();
+
+        //Chama o metodo conectar para estabelecer a conexão como BD
+        $conexao_bd->conectar();
+
+    }
+
     function Login($dados_logon){
 
-        $sql_select = "select * from tbl_clientes where nome_usuario ='".$dados_logon->$nome_usuario."'
-        and senha='".$dados_logon->$senha."'";
-        mysql_query($sql_select);
+        $sql_select = "select * from tbl_clientes where nome_usuario ='".$dados_logon->nome_usuario."'
+        and senha='".$dados_logon->senha."';";
+
+        $exec = mysql_query($sql_select);
+
+        $position = 0;
+
+        while($rs = mysql_fetch_array($exec)){
+
+          $_SESSION['id_cliente'] = $rs['id_cliente'];
+          $_SESSION['nome_cliente'] = $rs['nome_cliente'];
+          $_SESSION['sobrenome_cliente'] = $rs['sobrenome_cliente'];
+          $_SESSION['rg'] = $rs['rg'];
+          $_SESSION['cpf'] = $rs['cpf'];
+          $_SESSION['nome_usuario'] = $rs['nome_usuario'];
+          $_SESSION['senha'] = $rs['senha'];
+
+          $position .= 1;
+        }
+
+        require_once('views/cliente/index_cliente_view.php');
 
     }
 
