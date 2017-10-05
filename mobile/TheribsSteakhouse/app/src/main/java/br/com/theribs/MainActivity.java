28dce3a207be085b82a370137d70d1b84a441489
Giovanna Silva Resende <giovanna10silva@gmail.com>
2017-10-04 23:26:30 -0300

    algumas alterações de front

--- mobile/TheribsSteakhouse/app/src/main/java/br/com/theribs/MainActivity.java
+++ mobile/TheribsSteakhouse/app/src/main/java/br/com/theribs/MainActivity.java
@@ -19,19 +19,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
@@ -41,12 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
+
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -76,9 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    
-  
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
--- web/models/banco_classe.php
+++ web/models/banco_classe.php
@@ -26,7 +26,7 @@
 
             /*$this->server="10.107.134.33";*/
             /*$this->server="10.107.134.11";*/
-            $this->server="10.107.134.11";
+            $this->server="localhost";
             $this->user="root";
             $this->password="bcd127";
 
