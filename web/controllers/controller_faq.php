f87c01f9a35faab41439afd61424899d1cc59d69
GABRIEL LIMA DOS SANTOS <16165847@CFP127ALUNO.LOCAL>
2017-10-09 15:52:11 -0300

    Crud do FAQ (ADD,LIST, DELETE E VISU REGISTROS)

--- web/controllers/controller_faq.php
+++ web/controllers/controller_faq.php
@@ -21,6 +21,43 @@
           $faq_cadastro->Insert($faq_cadastro);
 
     }
+
+    public function Listar(){
+
+        require_once('models/faq_class.php');
+
+        $listFaq_Controller = new Faq();
+
+        return $listFaq_Controller->SelectAll();
+    }
+
+    public function Apagar(){
+
+        if($_SERVER['REQUEST_METHOD']=='GET'){
+
+            $id_faq=$_GET['id_faq'];
+
+            require_once('models/faq_class.php');
+
+            $deleteFaq_Controller = new Faq();
+
+            $deleteFaq_Controller->id_faq=$id_faq;
+            $deleteFaq_Controller->Delete($deleteFaq_Controller);
+        }
+    }
+
+    public function Buscar(){
+
+        $id_faq=$_GET['id_faq'];
+
+        require_once('models/faq_class.php');
+        $listFaq_Controller = new Faq();
+
+        $listFaq_Controller->id_faq=$id_faq;
+
+        $list = $listFaq_Controller->SelectById($listFaq_Controller);
+        require_once('views/cms/faq.php');
+    }
   }
 
  ?>
--- web/views/cms/faq.php
+++ web/views/cms/faq.php
@@ -1,4 +1,29 @@
+<?php
+    session_start();
 
+    $pergunta="";
+    $resposta="";
+    $status="0";
+    $nome_status="Desativada";
+
+    if(isset($_GET['modo'])){
+
+        if($_GET['modo'] == 'alterar'){
+
+            $id_faq=$list->id_faq;
+            $pergunta=$list->pergunta;
+            $resposta=$list->resposta;
+            $status=$list->status;
+
+            if($status=$list->status=1){
+                $nome_status="Ativada";
+            }else{
+              $nome_status="Desativada";
+            }
+        }
+    }
+
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,28 +35,67 @@
 
 <div id="container-cms">
     <?php require_once('header.php'); ?>
-
     <section>
         <?php require_once('menu.php'); ?>
 
-        <div id="conteudo-cms">
-          <section id="principal">
+        <section id="principal">
               <form name="frm_faq" action="router.php?controller=controller_faq&modo=salvar" method="post">
                 <div class="formulario">
-                    Pergunta<input type="text" name="txt_pergunta" value=""><br>
-                    Respota<input type="text" name="txt_resposta" value=""><br>
+                    Pergunta<input type="text" name="txt_pergunta" value="<?php echo($pergunta);?>"><br>
+                    Respota<input type="text" name="txt_resposta" value="<?php echo($resposta);?>"><br>
                     Status:<select name="slt_status">
-                      <option value="0" selected="">Desativada</option>
+                      <option value="<?php echo($status);?>" selected=""><?php echo($nome_status); ?></option>
                       <option value="1">Ativada</option>
                     </select>
                     <input type="submit" name="btn_faq">
                 </div>
               </form>
-              <div class="">
-                <?php //TODO ?>
+              <div class="consulta_faq">
+                <div class="titulo_consulta">
+                    FAQ's Cadastrados
+                </div>
+                <div class="faq_consulta">
+                    <table class="tabela_faq_consulta">
+                        <tr>
+                          <td class="titulo_tabela_faq_consulta">Pergunta</td>
+                          <td class="titulo_tabela_faq_consulta">Disponivel</td>
+                          <td class="titulo_tabela_faq_consulta">Opção</td>
+                        </tr>
+
+                          <?php
+
+                              require_once('controllers/controller_faq.php');
+
+                              $controller_Faq = new ControllerFaq();
+
+                              $rsFaq = $controller_Faq->Listar();
+
+                              $cont=0;
+
+                              while($cont<count($rsFaq)){
+                           ?>
+                        <tr>
+                          <td><?php  echo($rsFaq[$cont]->pergunta)?></td>
+                          <td><?php  echo($rsFaq[$cont]->status)?></td>
+                          <td>
+                            <a href="router.php?controller=controller_faq&modo=alterar&id_faq=<?php echo($rsFaq[$cont]->id_faq)?>">
+                                Visualizar
+                            </a>
+                              |
+                            <a href="router.php?controller=controller_faq&modo=apagar&id_faq=<?php echo($rsFaq[$cont]->id_faq)?>">
+                              Excluir
+                            </a>
+                          </td>
+                        </tr>
+                        <?php
+                              $cont+=1;
+                            }
+                         ?>
+                    </table>
+                </div>
               </div>
-          </section>
-        </div>
+        </section>
+
     </section>
     <footer>
         <?php require_once('rodape.php'); ?>
